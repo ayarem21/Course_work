7742f7e8f1e4027f8eb293c3e1375c74270225bb
ayarem21 <ayarem21@gmail.com>
2019-12-26 22:15:43 +0200

    bug fixed

--- app/models/seance.rb
+++ app/models/seance.rb
@@ -1,10 +1,4 @@
 class Seance < ApplicationRecord
-  belongs_to :film, dependent: :destroy
-  belongs_to :hall, dependent: :destroy
-  def self.search(search)
-    #where("#{Film.find(@film.film_id).name} LIKE ?", "%#{search}%")
-    Film.find(@seances.film_id).seances.joins(:films => :f).where("f.name LIKE ?", "%#{search}%")
-
-  end
-
+  belongs_to :film
+  belongs_to :hall
 end
